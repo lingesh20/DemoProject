64f790b58594f57a1668836d8716c88a2518c0fc
l.dhamodharan <l.dhamodharan@C11-THHI4770KVK.dir.svc.accenture.com>
2023-08-25 16:32:49 +0530

    first commit

---
+++ src/com/Demo/SayHello.java
@@ -0,0 +1,12 @@
+package com.Demo;
+
+public class SayHello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String name = "Lingesh";
+		System.out.println(name);
+	}
+
+}
